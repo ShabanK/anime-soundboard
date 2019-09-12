37902390ddbbdaaca1492f6318141f29d6e886c8
ShabanK <nooneyouwillknow@gmail.com>
2019-09-13 00:32:16 +0500

    well apparently i cant handle src attributes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,14 @@
 import React from "react";
+import jojo1 from "../bg/jojo1.jpg";
+
+// backgroundPosition: 'center',
+//   backgroundSize: 'cover',
+//   backgroundRepeat: 'no-repeat'
 
 const Layout = () => {
   return (
-    <div style={{ backgroundImage: "../bg/jojo1.jpg" }}>
-      <header className="header"> ANIME SOUNDBOARD </header>
+    <div style={{ backgroundImage: `url(${jojo1})` }}>
+      {/* <header className="header"> ANIME SOUNDBOARD </header> */}
     </div>
   );
 };
