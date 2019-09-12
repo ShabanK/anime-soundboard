268057d7ffe855a8459fbbc5d466f51cd76bcab5
ShabanK <nooneyouwillknow@gmail.com>
2019-09-13 00:08:11 +0500

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div style={{ backgroundImage: "../bg/jojo1.jpg" }}>
+      <header className="header"> ANIME SOUNDBOARD </header>
+    </div>
+  );
+};
+
+export default Layout;
